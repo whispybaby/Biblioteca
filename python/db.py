93dc1763687a029618688a153be22cace7320d62
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-07-05 22:07:19 -0400

    Conexión solucionada, no nos habíamos conectado

--- python/db.py
+++ python/db.py
@@ -1,17 +1,19 @@
 import pymysql
 from sys import exit
 
+
 def error(error):
     print(f"{error.args[0]}: {error.args[1]}")
 
+
 class ConexionBD:
-    def __init__(self, host, user, password, db) -> None:
+    def __init__(self, host, user, password, db):
         self.host = host
         self.user = user
         self.password = password
         self.db = db
 
-    def conectar_bd(self) -> str:
+    def conectar_bd(self):
         try:
             self.connection = pymysql.connect(
                 host=self.host,
@@ -37,14 +39,16 @@ class ConexionBD:
             self.cursor.callproc("sp_buscar_copia", [id_libro])
         except:
             pass
-    
+
     def sp_devolver_prestamo(self, id_prestamo):
         try:
-            self.cursor.execute(f"call sp_devolver_prestamo({id_prestamo}))")
+            self.cursor.execute(
+                f"call sp_devolver_prestamo({id_prestamo});")
             resultado = self.cursor.fetchone()
-            print(resultado)
+            print(resultado[0])
         except:
             pass
+
     def sp_prestar_copia(self, id_copia, id_usuario):
         try:
             self.cursor.callproc("sp_prestar_copia", [id_copia, id_usuario])
--- python/main.py
+++ python/main.py
@@ -1,51 +1,55 @@
 import db
-biblioteca = db.ConexionBD("localhost","python","python","biblioteca")
-print("Sistema de busqueda de libros porfavor introduce la operacion que deceas realizar")
+
 
 def menu():
-    print("1 - Solicitar plazo extra")
+    print("\n1 - Solicitar plazo extra")
     print("2 - Buscar copia")
     print("3 - Devolver copia ")
     print("4 - Prestar copia")
     print("5 - Salir")
 
+
+biblioteca = db.ConexionBD("localhost", "python", "python", "biblioteca")
+biblioteca.conectar_bd()
+print("Sistema de busqueda de libros porfavor introduce la operacion que deceas realizar")
+
 estado = True
 while(estado):
-     menu()
-     opcion = input("\n")
+    menu()
+    opcion = input("\n")
 
     # Plazo extra
-     if opcion == "1":
-          print("\nSolicitar plazo extra. ")
-          id_prestamo=input("Porfavor ingrese el id del prestamo:     ")
-          biblioteca.sp_plazo_extra(id_prestamo)
+    if opcion == "1":
+        print("\nSolicitar plazo extra. ")
+        id_prestamo = input("Porfavor ingrese el id del prestamo:     ")
+        biblioteca.sp_plazo_extra(id_prestamo)
 
-    # Buscar copia
-     elif opcion == "2":
-          print("\nBuscar copia.")
-          id_copia=input("Ingrese el id de la copia:     ")
-          biblioteca.sp_buscar_copia()
+        # Buscar copia
+    elif opcion == "2":
+        print("\nBuscar copia.")
+        id_copia = input("Ingrese el id de la copia:     ")
+        # biblioteca.sp_buscar_copia()
 
-    # Devolver prestamo
-     elif opcion == "3":
-          print("\nDevolver prestamo.")
-          id_prestamo=int(input("Ingrese el id del prestamo:      "))
-          biblioteca.sp_devolver_prestamo(id_prestamo)
-    
-    #Prestar copia
-     elif opcion == "4":
-          print("\nPrestar copia.")
-          copia = input("Copia: ")
-          biblioteca.sp_prestar_copia()
-     
-     #Pagar multa
-     elif opcion == "5":
-          print("\nPagar multa. ")
-          biblioteca.sp_pagar_multa()
-    # Salir
-     elif opcion == "5":
-          estado = False
-          print("\nSaliendo...")
+        # Devolver prestamo
+    elif opcion == "3":
+        print("\nDevolver prestamo.")
+        id_prestamo = int(input("Ingrese el id del prestamo: "))
+        biblioteca.sp_devolver_prestamo(id_prestamo)
 
-     else:
-          print("\nOpción no válida.")
+        # Prestar copia
+    elif opcion == "4":
+        print("\nPrestar copia.")
+        copia = input("Copia: ")
+        # biblioteca.sp_prestar_copia()
+
+        # Pagar multa
+    elif opcion == "5":
+        print("\nPagar multa. ")
+        # biblioteca.sp_pagar_multa()
+        # Salir
+    elif opcion == "5":
+        estado = False
+        print("\nSaliendo...")
+
+    else:
+        print("\nOpción no válida.")
