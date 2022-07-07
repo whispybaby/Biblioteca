3e2408a9c5b033f6eb13f426158870f9700e0b41
Gabriel Barrientos <gabri12.rubik@gmail.com>
2022-07-06 22:30:05 -0400

    Programa implementado en python

--- python/db.py
+++ python/db.py
@@ -1,5 +1,3 @@
-from subprocess import CalledProcessError, call
-from colorama import Cursor
 import pymysql
 from sys import exit
 
@@ -24,16 +22,22 @@ class ConexionBD:
                 db=self.db
             )
             self.cursor = self.connection.cursor()
-            print("Conexión exitosa.")
         except pymysql.Error as e:
             error(e)
             print("Conexión fallida, inténtelo de nuevo.")
             exit(1)
 
+    def guardar_cambios(self):
+        try:
+            self.connection.commit()
+            print('Cambios guardados.')
+        except:
+            pass
+
     def sp_plazo_extra(self, id_prestamo):
         try:
             self.cursor.callproc(
-                "sp_plazo_extra",[id_prestamo])
+                "sp_plazo_extra", [id_prestamo])
             resultado = self.cursor.fetchone()
 
             input(f"\n{resultado[0]}")
@@ -53,23 +57,23 @@ class ConexionBD:
     def sp_devolver_prestamo(self, id_prestamo):
         try:
             self.cursor.callproc(
-                "sp_devolver_prestamo" , [id_prestamo])
+                "sp_devolver_prestamo", [id_prestamo])
             resultado = self.cursor.fetchone()
-            
+
             input(f"\n{resultado[0]}")
         except:
             pass
 
-    def sp_prestar_copia(self, id_copia, id_usuario):
+    def sp_realizar_prestamo(self, id_copia, id_usuario):
         try:
             self.cursor.callproc(
-                "sp_prestar_copia", [id_copia, id_usuario])
+                "sp_realizar_prestamo", [id_copia, id_usuario])
             resultado = self.cursor.fetchone()
 
             input(f"\n{resultado[0]}")
         except:
             pass
-    
+
     def sp_pagar_multa(self, id_prestamo, monto_abono):
         try:
             self.cursor.callproc(
@@ -77,5 +81,203 @@ class ConexionBD:
             resultado = self.cursor.fetchone()
 
             input(f"\n{resultado[0]}")
+        except:
+            pass
+
+    def listar_usuarios(self):
+        try:
+            consulta = 'SELECT * FROM usuario;'
+            self.cursor.execute(consulta)
+            usuarios = self.cursor.fetchall()
+            for usuario in usuarios:
+                print(f'\nId usuario:      {usuario[0]}')
+                print(f'Nombre:          {usuario[1]}')
+                print(f'Apellido:        {usuario[2]}')
+                print(f'Id tipo usuario: {usuario[3]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_prestamos(self):
+        try:
+            consulta = 'SELECT * FROM prestamo;'
+            self.cursor.execute(consulta)
+            prestamos = self.cursor.fetchall()
+            for prestamo in prestamos:
+                print(f'\nId préstamo:    {prestamo[0]}')
+                print(f'Id copia:       {prestamo[1]}')
+                print(f'Id usuario:     {prestamo[2]}')
+                print(f'Id multa:       {prestamo[3]}')
+                print(f'Id plazo extra: {prestamo[4]}')
+                print(f'Fecha préstamo: {prestamo[5]}')
+                print(f'Fecha entrega:  {prestamo[6]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_autores(self):
+        try:
+            consulta = 'SELECT * FROM autor;'
+            self.cursor.execute(consulta)
+            autores = self.cursor.fetchall()
+            for autor in autores:
+                print(f'\nId autor:            {autor[0]}')
+                print(f'Nombre:              {autor[1]}')
+                print(f'Apellido:            {autor[2]}')
+                print(f'Fecha de nacimiento: {autor[3]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_estados(self):
+        try:
+            consulta = 'SELECT * FROM estado;'
+            self.cursor.execute(consulta)
+            estados = self.cursor.fetchall()
+            for estado in estados:
+                print(f'\nId estado: {estado[0]}')
+                print(f'Nombre:    {estado[1]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_libros(self):
+        try:
+            consulta = 'SELECT * FROM libro;'
+            self.cursor.execute(consulta)
+            libros = self.cursor.fetchall()
+            for libro in libros:
+                print(f'\nId libro:             {libro[0]}')
+                print(f'Id editorial:         {libro[1]}')
+                print(f'Título:               {libro[2]}')
+                print(f'Fecha de publicación: {libro[3]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_editoriales(self):
+        try:
+            consulta = 'SELECT * FROM editorial;'
+            self.cursor.execute(consulta)
+            editoriales = self.cursor.fetchall()
+            for editorial in editoriales:
+                print(f'\nId editorial:       {editorial[0]}')
+                print(f'Nombre:             {editorial[1]}')
+                print(f'Correo:             {editorial[2]}')
+                print(f'Teléfono:           {editorial[3]}')
+                print(f'Fecha de fundación: {editorial[4]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_categorias(self):
+        try:
+            consulta = 'SELECT * FROM categoria;'
+            self.cursor.execute(consulta)
+            categorias = self.cursor.fetchall()
+            for categoria in categorias:
+                print(f'\nId categoría: {categoria[0]}')
+                print(f'Nombre:       {categoria[1]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_copias(self):
+        try:
+            consulta = 'SELECT * FROM copia;'
+            self.cursor.execute(consulta)
+            copias = self.cursor.fetchall()
+            for copia in copias:
+                print(f'\nId copia:  {copia[0]}')
+                print(f'Id libro:  {copia[1]}')
+                print(f'Id idioma: {copia[2]}')
+                print(f'Id estado: {copia[3]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_idiomas(self):
+        try:
+            consulta = 'SELECT * FROM idioma;'
+            self.cursor.execute(consulta)
+            idiomas = self.cursor.fetchall()
+            for idioma in idiomas:
+                print(f'\nId idioma: {idioma[0]}')
+                print(f'Nombre:    {idioma[1]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_multas(self):
+        try:
+            consulta = 'SELECT * FROM multa;'
+            self.cursor.execute(consulta)
+            multas = self.cursor.fetchall()
+            for multa in multas:
+                print(f'\nId multa:        {multa[0]}')
+                print(f'Valor:           {multa[1]}')
+                print(f'Valor cancelado: {multa[2]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_plazos_extra(self):
+        try:
+            consulta = 'SELECT * FROM plazo_extra;'
+            self.cursor.execute(consulta)
+            plazos_extra = self.cursor.fetchall()
+            for plazo_extra in plazos_extra:
+                print(f'\nId plazo extra:  {plazo_extra[0]}')
+                print(f'Días extra:      {plazo_extra[1]}')
+                print(f'Veces extendido: {plazo_extra[2]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def listar_tipos_usuarios(self):
+        try:
+            consulta = 'SELECT * FROM tipo_usuario;'
+            self.cursor.execute(consulta)
+            tipos_usuarios = self.cursor.fetchall()
+            for tipo_usuario in tipos_usuarios:
+                print(f'\nId tipo usuario: {tipo_usuario[0]}')
+                print(f'Nombre:          {tipo_usuario[1]}')
+            print('\n', ' Fin submenú listados '.center(70, '='), sep='')
+        except:
+            pass
+
+    def sp_insertar_modificar_autor(self, id_autor, nombre, apellido, fecha_nacimiento):
+        try:
+            self.cursor.callproc('sp_insertar_modificar_autor', [
+                                 id_autor, nombre, apellido, fecha_nacimiento])
+            resultado = self.cursor.fetchone()
+            input(f'\n{resultado[0]}')
+        except:
+            pass
+
+    def sp_insertar_modificar_editorial(self, id_editorial, nombre, correo, telefono, fecha_fundacion):
+        try:
+            self.cursor.callproc('sp_insertar_modificar_editorial', [
+                                 id_editorial, nombre, correo, telefono, fecha_fundacion])
+            resultado = self.cursor.fetchone()
+            input(f'\n{resultado[0]}')
+        except:
+            pass
+
+    def sp_insertar_modificar_libro(self, id_libro, id_editorial, titulo, fecha_publicacion):
+        try:
+            self.cursor.callproc('sp_insertar_modificar_libro', [
+                                 id_libro, id_editorial, titulo, fecha_publicacion])
+            resultado = self.cursor.fetchone()
+            input(f'\n{resultado[0]}')
+        except:
+            pass
+
+    def sp_insertar_modificar_usuario(self, id_usuario, nombre, apellido, id_tipo_usuario):
+        try:
+            self.cursor.callproc('sp_insertar_modificar_usuario', [
+                                 id_usuario, nombre, apellido, id_tipo_usuario])
+            resultado = self.cursor.fetchone()
+            input(f'\n{resultado[0]}')
         except:
             pass
--- python/main.py
+++ python/main.py
@@ -1,71 +1,256 @@
 import db
 
 
-def menu():
-    print("\n1 - Solicitar plazo extra")
-    print("2 - Buscar copia")
-    print("3 - Devolver prestamo ")
-    print("4 - Prestar copia")
-    print("5 - Pagar multa")
-    print("6 - Ver opciones")
-    print("7 - Salir")
+def main():
+    biblioteca = db.ConexionBD('localhost', 'python', 'python', 'biblioteca')
+    biblioteca.conectar_bd()
 
-
-biblioteca = db.ConexionBD("localhost", "python", "python", "biblioteca")
-biblioteca.conectar_bd()
-print("Bienvenido al Sistema de busqueda de libros porfavor introduce la operacion que deceas realizar")
-
-estado = True
-while(estado):
+    print(' Sistema gestor de biblioteca '.center(70, '='))
     menu()
-    opcion = input("\nPorfavor ingrese una opcion disponible en el menu:      ")
 
-    # Plazo extra
-    if opcion == "1":
-        print("\n\nSolicitar plazo extra. ")
-        id_prestamo = input("Porfavor ingrese el id del prestamo:     ")
-        biblioteca.sp_plazo_extra(id_prestamo)
+    while True:
+        opcion = input('\nIngrese una opción: ')
+
+        # Ver opciones
+        if opcion == '?':
+            menu()
+
+        # Solicitar plazo extra
+        elif opcion == '1':
+            print('\n', ' Solicitar plazo extra '.center(70, '='), sep='')
+
+            id_prestamo = input('Id del préstamo: ')
+            try:
+                id_prestamo = int(id_prestamo)
+                if id_prestamo <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            biblioteca.sp_plazo_extra(id_prestamo)
 
         # Buscar copia
-    elif opcion == "2":
-        print("\n\nBuscar copia.")
-        id_libro = input("Ingrese el id del libro:     ")
-        biblioteca.sp_buscar_copia(id_libro)
-   
+        elif opcion == '2':
+            print('\n', ' Buscar copia '.center(70, '='), sep='')
+
+            id_libro = input('Id del libro: ')
+            try:
+                id_libro = int(id_libro)
+                if id_libro <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            biblioteca.sp_buscar_copia(id_libro)
 
         # Devolver prestamo
-    elif opcion == "3":
-        print("\n\nDevolver prestamo.")
-        id_prestamo = int(input("Ingrese el id del prestamo: "))
-        biblioteca.sp_devolver_prestamo(id_prestamo)
-        
+        elif opcion == '3':
+            print('\n', ' Devolver préstamo '.center(70, '='), sep='')
 
-        # Prestar copia
-    elif opcion == "4":
-        print("\n\nPrestar copia.")
-        id_copia = int(input("Porfavor ingrese el id de la copia:   "))
-        id_usuario = int(input("Porfavor ingrese el id del usuario:     "))
-        biblioteca.sp_prestar_copia(id_copia, id_usuario)
-     
+            id_prestamo = input('Id del préstamo: ')
+            try:
+                id_prestamo = int(id_prestamo)
+                if id_prestamo <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            biblioteca.sp_devolver_prestamo(id_prestamo)
+
+        # Realizar préstamo
+        elif opcion == '4':
+            print('\n', ' Realizar préstamo '.center(70, '='), sep='')
+
+            id_copia = input('Id de la copia: ')
+            try:
+                id_copia = int(id_copia)
+                if id_copia <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            id_usuario = input('Id del usuario: ')
+            try:
+                id_usuario = int(id_usuario)
+                if id_usuario <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            biblioteca.sp_realizar_prestamo(id_copia, id_usuario)
+
         # Pagar multa
-    elif opcion == "5":
-        print("\n\nPagar multa. ")
-        id_prestamo = int(input("Ingrese id de prestamo:    "))
-        monto_abono = int(input("Ingrese el monto a abonar: "))
-        biblioteca.sp_pagar_multa(id_prestamo, monto_abono)
-      
-    elif opcion == "6":
-        print("\n\n - Solicitar plazo extra sirve para solicitar un plazo extra al libro  el cual es otorgado segun el usuario que se posea")
-        print("2 - Buscar copia esta opcion sirve para buscar a copia segun el id_ del la copia del libro")
-        print("3 - Devolver prestamo esta opcion sirve cuando se hace registros de que la copia del libro fue regresada con exito")
-        print("4 - Prestar copia es una opcion que sirve para lllevar a cabo el registro al prestar una copia del libro")
-        print("5 - Pagar multa este proceso sirve para cancelar la deuda de los dias pedientes")
-        print("6 - Ver opciones esta opcion sirve para ver las opciones del menu mas definidas")
-        print("7 - Salir metodo el cual sirve para salir del programa")
-        # Salir
-    elif opcion == "7":
-        estado = False
-        print("\nSaliendo...")
+        elif opcion == '5':
+            print('\n', ' Pagar multa '.center(70, '='), sep='')
 
-    else:
-        print("\n\nOpción no válida.")
+            id_prestamo = input('Id del préstamo: ')
+            try:
+                id_prestamo = int(id_prestamo)
+                if id_prestamo <= 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl id debe ser un número positivo.')
+                continue
+
+            monto_abono = input('Monto a abonar: ')
+            try:
+                monto_abono = int(monto_abono)
+                if monto_abono < 0:
+                    raise ValueError
+            except ValueError:
+                print('\nEl monto a abonar debe ser positivo')
+                continue
+
+            biblioteca.sp_pagar_multa(id_prestamo, monto_abono)
+
+        # Añadir/modificar registros
+        elif opcion == '6':
+            print('\n', ' Submenú añadir/modificar registros '.center(70, '='), sep='')
+            menu_añadir_modificar()
+            opcion_añadir_modificar = input('\nIngrese una opción: ')
+
+            # Autor
+            if opcion_añadir_modificar == '1':
+                id_autor = input('\nId autor:            ')
+                nombre = input('Nombre:              ')
+                apellido = input('Apellido:            ')
+                fecha_nacimiento = input('Fecha de nacimiento: ')
+                biblioteca.sp_insertar_modificar_autor(id_autor, nombre, apellido, fecha_nacimiento)
+
+                print('\n', ' Fin submenú añadir/modificar registros '.center(70, '='), sep='')
+
+            # Editorial
+            elif opcion_añadir_modificar == '2':
+                id_editorial = input('\nId editorial:    ')
+                nombre = input('Nombre:          ')
+                correo = input('Correo:          ')
+                telefono = input('Teléfono:        ')
+                fecha_fundacion = input('Fecha fundación: ')
+                biblioteca.sp_insertar_modificar_editorial(id_editorial, nombre, correo, telefono, fecha_fundacion)
+
+                print('\n', ' Fin submenú añadir/modificar registros '.center(70, '='), sep='')
+
+            # Libro
+            elif opcion_añadir_modificar == '3':
+                id_libro = input('\nId libro:             ')
+                id_editorial = input('Id editorial:         ')
+                titulo = input('Título:               ')
+                fecha_publicacion = input('Fecha de publicación: ')
+                biblioteca.sp_insertar_modificar_libro(id_libro, id_editorial, titulo, fecha_publicacion)
+
+                print('\n', ' Fin submenú añadir/modificar registros '.center(70, '='), sep='')
+
+            # Usuario
+            elif opcion_añadir_modificar == '4':
+                id_usuario = input('\nId usuario:      ')
+                nombre = input('Nombre:          ')
+                apellido =  input('Apellido:        ')
+                id_tipo_usuario = input('Id tipo usuario: ')
+                biblioteca.sp_insertar_modificar_usuario(id_usuario, nombre, apellido, id_tipo_usuario)
+
+                print('\n', ' Fin submenú añadir/modificar registros '.center(70, '='), sep='')
+
+            else:
+                print('\nOpción no válida.')
+
+        # Listar registros
+        elif opcion == '7':
+            print('\n', ' Submenú listados '.center(70, '='), sep='')
+            menu_registros()
+            opcion_registros = input('\nIngrese una opción: ')
+
+            # Libros
+            if opcion_registros == '1':
+                biblioteca.listar_libros()
+
+            elif opcion_registros == '2':
+                biblioteca.listar_usuarios()
+
+            elif opcion_registros == '3':
+                biblioteca.listar_prestamos()
+
+            elif opcion_registros == '4':
+                biblioteca.listar_autores()
+
+            elif opcion_registros == '5':
+                biblioteca.listar_estados()
+
+            elif opcion_registros == '6':
+                biblioteca.listar_editoriales()
+
+            elif opcion_registros == '7':
+                biblioteca.listar_categorias()
+
+            elif opcion_registros == '8':
+                biblioteca.listar_copias()
+
+            elif opcion_registros == '9':
+                biblioteca.listar_idiomas()
+
+            elif opcion_registros == '10':
+                biblioteca.listar_multas()
+
+            elif opcion_registros == '11':
+                biblioteca.listar_plazos_extra()
+
+            elif opcion_registros == '12':
+                biblioteca.listar_tipos_usuarios()
+
+            else:
+                print('\nOpción no válida.')
+
+        elif opcion == '8':
+            print('\n', ' Confirmar cambios '.center(70, '='), sep='')
+
+            desicion = input('Sí, si o s para confirmar los cambios: ')
+            if desicion.lower() in ('sí', 'si', 's'):
+                biblioteca.guardar_cambios()
+
+        # Salir
+        elif opcion == '9':
+            print('\nSaliendo...')
+            break
+
+        else:
+            print('\n\nOpción no válida.')
+
+
+def menu():
+    print('\n? - Ver las opciones')
+    print('1 - Solicitar plazo extra')
+    print('2 - Buscar copia')
+    print('3 - Devolver prestamo ')
+    print('4 - Realizar préstamo')
+    print('5 - Pagar multa')
+    print('6 - Añadir/modificar registros')
+    print('7 - Listar registros')
+    print('8 - Guardar los cambios')
+    print('9 - Salir')
+
+def menu_registros():
+    print('\n1  - Libros')
+    print('2  - Usuarios')
+    print('3  - Préstamos')
+    print('4  - Autores')
+    print('5  - Estados')
+    print('6  - Editoriales')
+    print('7  - Categorías')
+    print('8  - Copias')
+    print('9  - Idiomas')
+    print('10 - Multas')
+    print('11 - Plazos extra')
+    print('12 - Tipos de usuario')
+
+def menu_añadir_modificar():
+    print('\n1  - Autores')
+    print('2  - Editoriales')
+    print('3  - Libros')
+    print('4  - Usuarios')
+
+if __name__ == '__main__':
+    main()
